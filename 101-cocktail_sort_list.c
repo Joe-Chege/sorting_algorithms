37b2e88a16e1f49501683a078e64280b2c63dd2a
Joe-Chege <njorogejosephchege@gmail.com>
2023-07-21 14:41:01 +0300

    Sort an array of integers in ascending order using the selection sort algorithm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,80 +1,90 @@
 #include "sort.h"
 
 /**
- * switch_nodes - Function to swap two adjacent nodes in a doubly linked list
- * @list: Pointer to the head of the doubly linked list
- * @p: Pointer to the node to be swapped with the following node
+ * list_len - function returns length of list
+ * @list: head of list
+ *
+ * Return: length
  */
-void switch_nodes(listint_t **list, listint_t **p)
+size_t list_len(listint_t *list)
 {
-    listint_t *one, *two, *three, *four;
+	size_t len = 0;
 
-    one = (*p)->prev;
-    two = *p;
-    three = (*p)->next;
-    four = (*p)->next->next;
-
-    two->next = four;
-    if (four)
-        four->prev = two;
-
-    three->next = two;
-    three->prev = two->prev;
-
-    if (one)
-        one->next = three;
-    else
-        *list = three;
-
-    two->prev = three;
-    *p = three;
+	while (list)
+	{
+		len++;
+		list = list->next;
+	}
+	return (len);
 }
 
 /**
- * cocktail_sort_list - Sorts a doubly linked list of integers in ascending order
- *                      using the Cocktail Shaker sort algorithm
- * @list: Pointer to the head of the doubly linked list
+ * switch_nodes - function swaps nodes at pointer p with the following node
+ * @list: head of list
+ * @p: pointer to node
+ */
+void switch_nodes(listint_t **list, listint_t **p)
+{
+	listint_t *one, *two, *three, *four;
+
+	one = (*p)->prev;
+	two = *p;
+	three = (*p)->next;
+	four = (*p)->next->next;
+	two->next = four;
+	if (four)
+		four->prev = two;
+	three->next = two;
+	three->prev = two->prev;
+	if (one)
+		one->next = three;
+	else
+		*list = three;
+	two->prev = three;
+	*p = three;
+}
+
+/**
+ *  cocktail_sort_list - function sorts a doubly linked list using
+ * the cocktail sort algorithm
+ * @list: pointer to list
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *p;
-    int sorted = 0;
+	listint_t *p;
+	int sorted = 0;
 
-    if (!list || !*list || list_len(*list) < 2)
-        return;
-
-    p = *list;
-
-    while (!sorted)
-    {
-        sorted = 1;
-        while (p->next)
-        {
-            if (p->n > p->next->n)
-            {
-                sorted = 0;
-                switch_nodes(list, &p);
-                print_list(*list);
-            }
-            else
-                p = p->next;
-        }
-
-        if (sorted)
-            break;
-
-        p = p->prev;
-        while (p->prev)
-        {
-            if (p->n < p->prev->n)
-            {
-                sorted = 0;
-                p = p->prev;
-                switch_nodes(list, &p);
-                print_list(*list);
-            }
-            else
-                p = p->prev;
-        }
-    }
+	if (!list || !*list || list_len(*list) < 2)
+		return;
+	p = *list;
+	while (!sorted)
+	{
+		sorted = 1;
+		while (p->next)
+		{
+			if (p->n > p->next->n)
+			{
+				sorted = 0;
+				switch_nodes(list, &p);
+				print_list(*list);
+			}
+			else
+				p = p->next;
+		}
+		if (sorted)
+			break;
+		p = p->prev;
+		while (p->prev)
+		{
+			if (p->n < p->prev->n)
+			{
+				sorted = 0;
+				p = p->prev;
+				switch_nodes(list, &p);
+				print_list(*list);
+			}
+			else
+				p = p->prev;
+		}
+	}
 }
